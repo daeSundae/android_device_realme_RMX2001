da5f0192df5d9ee2720e162331a2ce84267d6b95
daeSundae <yusufomer2005@hotmail.com>
2021-05-19 12:46:53 +0300

    RMX2001: Add SHRP build flags

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -149,3 +149,23 @@ TARGET_USES_LOGD := true
 
 # PBRP Build Flags
 PB_DISABLE_DEFAULT_TREBLE_COMP := true
+
+# NOTE - Dont use '-' or blank spaces in flag values , otherwise it will create build errors or other bugs in recovery (Excluding SHRP_PATH,SHRP_REC). 
+# Path of your SHRP Tree
+SHRP_PATH := device/realme/RMX2001
+# Maintainer name *
+SHRP_MAINTAINER := dae
+# Device codename *
+SHRP_DEVICE_CODE := RMX2001
+# Recovery Type (It can be treble,normal,SAR) [Only for About Section] *
+SHRP_REC_TYPE := SAR
+# Recovery Type (It can be A/B or A_only) [Only for About Section] *
+SHRP_DEVICE_TYPE := A_only
+# EDL
+SHRP_EDL_MODE := 1
+SHRP_EXTERNAL := /external_sd
+SHRP_INTERNAL := /sdcard
+SHRP_OTG := /usbotg
+SHRP_FLASH := 1
+# Path
+SHRP_REC := /dev/block/by-name/recovery
